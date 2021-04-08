3d6250d3ea4f441ff818964d4ad0c74feba72054
ITAAS <ajatupong@hotmail.com>
2021-04-08 15:44:10 +0700

    update stock account

---
+++ itaas_create_stock_account/models/stock_move.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019-present  Technaureus Info Solutions(<http://www.technaureus.com/>).
+
+from odoo import models, fields, api
+from openerp import api, fields, models, _
+from openerp.osv import expression
+from openerp.tools import float_is_zero
+from openerp.tools import float_compare, float_round
+from openerp.tools.misc import formatLang
+from openerp.exceptions import UserError, ValidationError
+from datetime import datetime, date
+
+
+import time
+import math
+
+
+class stock_move(models.Model):
+    _inherit = "stock.move"
+
+    def update_account_entry_action_done(self):
+
+        am_id = self.env['account.move'].search([('stock_move_id','=',self.id)],limit=1)
+        if not am_id:
+            # Init a dict that will group the moves by valuation type, according to `move._is_valued_type`.
+            valued_moves = {valued_type: self.env['stock.move'] for valued_type in self._get_valued_types()}
+            for move in self:
+                if float_is_zero(move.quantity_done, precision_rounding=move.product_uom.rounding):
+                    continue
+                for valued_type in self._get_valued_types():
+                    if getattr(move, '_is_%s' % valued_type)():
+                        valued_moves[valued_type] |= move
+                        continue
+
+            # AVCO application
+            #valued_moves['in'].product_price_update_before_done()
+
+            #res = super(stock_move, self)._action_done(cancel_backorder=cancel_backorder)
+
+            # '_action_done' might have created an extra move to be valued
+            for move in self:
+                for valued_type in self._get_valued_types():
+                    if getattr(move, '_is_%s' % valued_type)():
+                        valued_moves[valued_type] |= move
+                        continue
+
+            stock_valuation_layers = self.env['stock.valuation.layer'].sudo()
+            # Create the valuation layers in batch by calling `moves._create_valued_type_svl`.
+            for valued_type in self._get_valued_types():
+                todo_valued_moves = valued_moves[valued_type]
+                if todo_valued_moves:
+                    todo_valued_moves._sanity_check_for_valuation()
+                    stock_valuation_layers |= getattr(todo_valued_moves, '_create_%s_svl' % valued_type)()
+                    continue
+
+            for svl in stock_valuation_layers.with_context(active_test=False):
+                if not svl.product_id.valuation == 'real_time':
+                    continue
+                if svl.currency_id.is_zero(svl.value):
+                    continue
+                svl.stock_move_id._account_entry_move(svl.quantity, svl.description, svl.id, svl.value)
