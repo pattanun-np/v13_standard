e59bdabe3637cf7f0791adb91364dbad17f5d217
ITAAS <ajatupong@hotmail.com>
2021-04-08 15:44:10 +0700

    update stock account

---
+++ itaas_create_stock_account/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2016-Today  ITAAS (<http://www.itaas.co.th/>).
+# fix เรื่อง reverse move ของ V13
+{
+    "name": "Itaas Cheque Return",
+    'version': '13.0.1.1',
+    "category": 'itaas',
+    'summary': 'Itaas Cheque Return.',
+    "description": """
+        .
+    """,
+    "sequence": 1,
+    "author": "IT as a Service Co., Ltd.",
+    "website": "http://www.itaas.co.th/",
+    "version": '1.0',
+    "depends": ['base','account','stock','stock_account'],
+    "data": [
+        'views/stock_move_view.xml',
+
+    ],
+    'qweb': [],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
